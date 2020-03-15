7b71b8f2c4a19f9de91e76bd098f658de7d5302d
maximbircu <maximbircu@gmail.com>
2020-03-15 17:24:23 +0200

    Cover DevToolsParser with unit tests

--- devtools/common/src/commonMain/kotlin/com/maximbircu/devtools/common/DevToolsParser.kt
+++ devtools/common/src/commonMain/kotlin/com/maximbircu/devtools/common/DevToolsParser.kt
@@ -1,18 +1,21 @@
 package com.maximbircu.devtools.common
 
 import com.maximbircu.devtools.common.core.DevTool
-import com.maximbircu.devtools.common.core.reader.DevToolsReader
 import com.maximbircu.devtools.common.core.reader.DevToolsSource
 
-class DevToolsParser(private val readers: List<DevToolsReader>) {
+interface DevToolsParser {
+    fun getDevTools(): Map<String, DevTool<Any>>
+
     companion object {
         fun create(devToolsSources: List<DevToolsSource>): DevToolsParser {
-            return DevToolsParser(devToolsSources.map { it.getReader() })
+            return DevToolsParserImpl(devToolsSources)
         }
     }
+}
 
-    fun getDevTools(): Map<String, DevTool<Any>> = readers.fold(
-        mapOf(),
-        { accumulator, devToolsReader -> accumulator + devToolsReader.getDevTools() }
-    )
+private class DevToolsParserImpl(private val sources: List<DevToolsSource>): DevToolsParser {
+    override fun getDevTools(): Map<String, DevTool<Any>> {
+        val readers = sources.map { it.getReader() }
+        return readers.fold(mapOf(), { accumulator, reader -> accumulator + reader.getDevTools() })
+    }
 }
