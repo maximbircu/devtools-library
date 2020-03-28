6023e0403ccaa68f2c38f57779c6da5fad9e1844
Maxim Bircu <maximbircu@gmail.com>
2020-03-28 21:07:41 +0200

    Cover memmory tool source with unit tests

--- devtools/android/src/main/kotlin/com/maximbircu/devtools/android/readers/DevToolsSources.kt
+++ devtools/android/src/main/kotlin/com/maximbircu/devtools/android/readers/DevToolsSources.kt
@@ -3,19 +3,15 @@ package com.maximbircu.devtools.android.readers
 import com.maximbircu.devtools.common.core.DevTool
 import com.maximbircu.devtools.common.core.reader.DevToolsReader
 import com.maximbircu.devtools.common.core.reader.DevToolsSource
-import com.maximbircu.devtools.common.presentation.tools.toggle.ToggleTool
 import com.maximbircu.devtools.common.readers.DevToolsSources
 
+fun DevToolsSources.memory(devTools: Map<String, DevTool<*>>): DevToolsSource {
+    return MemoryDevToolsSource(devTools)
+}
+
 @Suppress("UNCHECKED_CAST")
-fun DevToolsSources.memory(): DevToolsSource = object : DevToolsSource {
-    override fun getReader(): DevToolsReader {
-        return object : DevToolsReader {
-            override fun getDevTools(): Map<String, DevTool<Any>> {
-                val toggleTool = ToggleTool(defaultValue = false)
-                toggleTool.title = "Toggle dev tool"
-                toggleTool.key = "toggle-tool"
-                return mapOf("toggle-tool" to toggleTool) as Map<String, DevTool<Any>>
-            }
-        }
+private class MemoryDevToolsSource(private val devTools: Map<String, DevTool<*>>) : DevToolsSource {
+    override fun getReader(): DevToolsReader = object : DevToolsReader {
+        override fun getDevTools() = devTools as Map<String, DevTool<Any>>
     }
 }
--- samples/android/src/main/kotlin/com/maximbircu/devtools/MainActivity.kt
+++ samples/android/src/main/kotlin/com/maximbircu/devtools/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.maximbircu.devtools.android.DevToolsConfigurationScreen
 import com.maximbircu.devtools.android.readers.memory
 import com.maximbircu.devtools.common.DevTools
+import com.maximbircu.devtools.common.presentation.tools.toggle.ToggleTool
 import com.maximbircu.devtools.common.readers.DevToolsSources
 import kotlinx.android.synthetic.main.activity_main.devToolsContainer
 
@@ -15,7 +16,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val source = DevToolsSources.memory()
+        val toggleTool = ToggleTool(defaultValue = false)
+        toggleTool.title = "Toggle dev tool"
+        toggleTool.key = "toggle-tool"
+        val source = DevToolsSources.memory(mapOf("toggle-tool" to toggleTool))
         devtools = DevTools.create(source)
 
         DevToolsConfigurationScreen.attachToView(devToolsContainer, devtools)
