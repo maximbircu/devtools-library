62f155f30f4a925d9679540bc1622d1a0add2ace
Maxim Bircu <maximbircu@gmail.com>
2020-03-29 01:48:41 +0200

    Cover YAML source and reader with unit tests

--- devtools/android/src/test/kotlin/com/maximbircu/devtools/android/readers/DevToolsSourcesKtTest.kt
+++ devtools/android/src/test/kotlin/com/maximbircu/devtools/android/readers/DevToolsSourcesKtTest.kt
@@ -2,8 +2,11 @@ package com.maximbircu.devtools.android.readers
 
 import com.maximbircu.devtools.android.BaseTest
 import com.maximbircu.devtools.android.readers.soruces.MemoryDevToolsSource
+import com.maximbircu.devtools.android.readers.soruces.yaml.YamlDevToolsSource
+import com.maximbircu.devtools.android.utils.mockk
 import com.maximbircu.devtools.common.readers.DevToolsSources
 import org.junit.Test
+import java.io.InputStream
 import kotlin.test.assertTrue
 
 class DevToolsSourcesKtTest : BaseTest() {
@@ -12,5 +15,19 @@ class DevToolsSourcesKtTest : BaseTest() {
         val source = DevToolsSources.memory(emptyMap())
 
         assertTrue(source is MemoryDevToolsSource)
+    }
+
+    @Test
+    fun `returns an yaml dev tool source given input stream`() {
+        val source = DevToolsSources.yaml(mockk(), mockk<InputStream>())
+
+        assertTrue(source is YamlDevToolsSource)
+    }
+
+    @Test
+    fun `returns an yaml dev tool source given file name`() {
+        val source = DevToolsSources.yaml(mockk(relaxed = true), "dev-tools.yml")
+
+        assertTrue(source is YamlDevToolsSource)
     }
 }
