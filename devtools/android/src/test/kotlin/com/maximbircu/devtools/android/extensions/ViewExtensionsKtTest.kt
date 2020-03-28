25be8dc3d56ab56e4bb9886d6bc1bff36f7884f8
Maxim Bircu <maximbircu@gmail.com>
2020-03-28 20:52:27 +0200

    Cover view extensions with unit tests

--- devtools/android/src/test/kotlin/com/maximbircu/devtools/android/extensions/ViewExtensionsKtTest.kt
+++ devtools/android/src/test/kotlin/com/maximbircu/devtools/android/extensions/ViewExtensionsKtTest.kt
@@ -1,14 +1,19 @@
 package com.maximbircu.devtools.android.extensions
 
 import android.view.View
+import android.view.View.OnClickListener
+import android.view.ViewGroup
 import com.maximbircu.devtools.android.BaseTest
 import com.maximbircu.devtools.android.utils.mockk
+import io.mockk.every
+import io.mockk.slot
 import io.mockk.verify
 import org.junit.Test
+import kotlin.test.assertEquals
 
 class ViewExtensionsKtTest : BaseTest() {
     @Test
-    fun `shows view`() {
+    fun `sets view visibility to visible`() {
         val view: View = mockk()
 
         view.show()
@@ -17,7 +22,7 @@ class ViewExtensionsKtTest : BaseTest() {
     }
 
     @Test
-    fun `hides view`() {
+    fun `sets view visibility to gone`() {
         val view: View = mockk()
 
         view.hide()
@@ -26,12 +31,44 @@ class ViewExtensionsKtTest : BaseTest() {
     }
 
     @Test
+    fun `returns proper views`() {
+        val children = listOf<View>(mockk(), mockk())
+        val rootView = createViewGroup(children)
+
+        assertEquals(children, rootView.children().toList())
+    }
+
+    @Test
+    @Suppress("UNCHECKED_CAST")
     fun `sets click listener`() {
         val view: View = mockk()
+        val slot = slot<OnClickListener>()
+        every { view.setOnClickListener(capture(slot)) } answers { slot.captured.onClick(mockk()) }
         val listener = {}
 
         view.setOnClickListener(listener)
 
-        verify { view.setOnClickListener(any()) }
+        verify {
+            view.setOnClickListener(any())
+            listener.invoke()
+        }
+    }
+
+    @Test
+    fun `sets enabled all views recursively`() {
+        val children = listOf<View>(createViewGroup(), mockk())
+        val rootView = createViewGroup(children)
+
+        rootView.setEnabledRecursively(false)
+
+        verify { rootView.isEnabled = false }
+        children.forEach { verify { it.isEnabled = false } }
+    }
+
+    private fun createViewGroup(views: List<View> = emptyList()): ViewGroup {
+        val view = mockk<ViewGroup>(relaxed = true)
+        every { view.childCount } returns(views.size)
+        views.forEachIndexed { index, child -> every { view.getChildAt(index) }.returns(child) }
+        return view
     }
 }
