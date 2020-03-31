89ef117b7b815a244f589115c73bea1e6643f14d
Maxim Bircu <maximbircu@gmail.com>
2020-03-31 16:10:27 +0300

    Fix YamlDevToolsTypesRegistryImplTest

--- devtools/android/src/test/kotlin/com/maximbircu/devtools/android/readers/soruces/yaml/YamlDevToolsTypesRegistryImplTest.kt
+++ devtools/android/src/test/kotlin/com/maximbircu/devtools/android/readers/soruces/yaml/YamlDevToolsTypesRegistryImplTest.kt
@@ -2,6 +2,7 @@ package com.maximbircu.devtools.android.readers.soruces.yaml
 
 import com.maximbircu.devtools.android.BaseTest
 import com.maximbircu.devtools.common.presentation.tools.toggle.ToggleTool
+import io.mockk.CapturingSlot
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.mockkConstructor
@@ -9,25 +10,34 @@ import io.mockk.slot
 import io.mockk.verify
 import org.junit.Test
 import org.yaml.snakeyaml.TypeDescription
+import org.yaml.snakeyaml.Yaml
 import org.yaml.snakeyaml.constructor.Constructor
+import java.io.InputStream
 import kotlin.test.assertEquals
 
 class YamlDevToolsTypesRegistryImplTest : BaseTest() {
     @Test
     fun `registers type descriptors`() {
-        val typesRegistryData = """
-            com.maximbircu.devtools.common.presentation.tools.toggle.ToggleTool : "!toggle"
-        """.trimIndent().byteInputStream()
-        val typeDescriptionSlot = slot<TypeDescription>()
-        mockkConstructor(Constructor::class)
-        every {
-            anyConstructed<Constructor>().addTypeDescription(capture(typeDescriptionSlot))
-        } returns mockk(relaxed = true)
+        val typeDescriptionSlot: CapturingSlot<TypeDescription> = slot()
+        setUpConstructor(typeDescriptionSlot)
+        setUpYaml(mapOf(ToggleTool::class.java.name to "!toggle"))
 
-        YamlDevToolsTypesRegistry.create(typesRegistryData)
+        YamlDevToolsTypesRegistry.create("".trimIndent().byteInputStream())
 
         assertEquals(ToggleTool::class.java, typeDescriptionSlot.captured.type)
         assertEquals("!toggle", typeDescriptionSlot.captured.tag.value)
         verify(exactly = 1) { anyConstructed<Constructor>().addTypeDescription(any()) }
+    }
+
+    private fun setUpConstructor(slot: CapturingSlot<TypeDescription>) {
+        mockkConstructor(Constructor::class)
+        every {
+            anyConstructed<Constructor>().addTypeDescription(capture(slot))
+        } returns mockk(relaxed = true)
+    }
+
+    private fun setUpYaml(types: Map<String, String>) {
+        mockkConstructor(Yaml::class)
+        every { anyConstructed<Yaml>().load<Map<String, String>>(any<InputStream>()) } returns types
     }
 }
