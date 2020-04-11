aba75aeb20bdf13e7b2e355b8ca9fc595a6ccdc2
Alexandr Vdovicenco <vdovicenco.alexandr@gmail.com>
2020-04-12 02:59:28 +0300

    Init sample ios app with dependency manger config

---
+++ samples/ios/DevToolsApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  DevToolsApp
+//
+//  Created by Alexandr Vdovicenco on 4/12/20.
+//  Copyright © 2020 com.devtools.multiplatform. All rights reserved.
+//
+
+import UIKit
+import DevtoolsFramework
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ samples/ios/Pods/Target Support Files/Pods-DevToolsAppTests/Pods-DevToolsAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DevToolsAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_DevToolsAppTests
+@end
