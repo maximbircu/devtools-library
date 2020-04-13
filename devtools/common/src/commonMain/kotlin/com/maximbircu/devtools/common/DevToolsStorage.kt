9996f2a70f68a7c103117ef8fc6b7c4431090ceb
Maxim Bircu <maximbircu@gmail.com>
2020-04-13 13:53:07 +0300

    Cover DevToolsStorage.getGroup with unit tests

--- devtools/common/src/commonMain/kotlin/com/maximbircu/devtools/common/DevToolsStorage.kt
+++ devtools/common/src/commonMain/kotlin/com/maximbircu/devtools/common/DevToolsStorage.kt
@@ -20,6 +20,12 @@ interface DevToolsStorage {
      */
     fun <T> getValue(key: String): T
 
+    /**
+     * Provides a [DevToolsStorage] which contains all tools group members.
+     *
+     * @param key the group unique id [GroupTool.key]
+     * @return a [DevToolsStorage] which contains the [GroupTool.tools] data
+     */
     fun getGroup(key: String): DevToolsStorage
 
     /**
