e57fef2e7f207a95e3e52eaa637c798f90434a7b
Alexandr Vdovicenco <vdovicenco.alexandr@gmail.com>
2020-04-22 21:18:09 +0300

    Add ios build and test action default work dir

--- devtools/ios/DevtoolsFrameworkTests/DevtoolsFrameworkTests.swift
+++ devtools/ios/DevtoolsFrameworkTests/DevtoolsFrameworkTests.swift
@@ -3,11 +3,11 @@ import XCTest
 
 class DevtoolsFrameworkTests: XCTestCase {
 
-    override func setUpWithError() throws {
+    override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
-    override func tearDownWithError() throws {
+    override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
