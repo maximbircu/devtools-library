552fb3662891251a8e38ac9478132814a896926e
Maxim Bircu <maximbircu@gmail.com>
2020-05-02 13:18:18 +0300

    Suppress ApplySharedPref

--- samples/android/src/main/kotlin/com/maximbircu/devtools/customtool/MyCustomToolStore.kt
+++ samples/android/src/main/kotlin/com/maximbircu/devtools/customtool/MyCustomToolStore.kt
@@ -1,5 +1,6 @@
 package com.maximbircu.devtools.customtool
 
+import android.annotation.SuppressLint
 import android.content.Context
 import com.maximbircu.devtools.SampleApplication
 import com.maximbircu.devtools.common.core.ToolStore
@@ -9,6 +10,7 @@ private val sharedPrefs = SampleApplication.application.getSharedPreferences(
     Context.MODE_PRIVATE
 )
 
+@SuppressLint("ApplySharedPref")
 class MyCustomToolStore(private val tool: MyCustomTool): ToolStore<Boolean> {
     override var isEnabled: Boolean
         get() = sharedPrefs.getBoolean("is_enabled_${tool.key}", tool.defaultEnabledValue)
