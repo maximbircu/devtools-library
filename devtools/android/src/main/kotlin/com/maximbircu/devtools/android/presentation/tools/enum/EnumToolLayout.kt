9aa6e822643e52243f01332fd133c5c9c7d903a2
Maxim Bircu <maximbircu@gmail.com>
2020-05-04 01:06:04 +0300

    Use one container for both compact and current value views

--- devtools/android/src/main/kotlin/com/maximbircu/devtools/android/presentation/tools/enum/EnumToolLayout.kt
+++ devtools/android/src/main/kotlin/com/maximbircu/devtools/android/presentation/tools/enum/EnumToolLayout.kt
@@ -1,6 +1,8 @@
 package com.maximbircu.devtools.android.presentation.tools.enum
 
 import android.content.Context
+import android.view.View
+import android.widget.TextView
 import com.maximbircu.devtools.android.R
 import com.maximbircu.devtools.android.extensions.setOnClickListener
 import com.maximbircu.devtools.android.presentation.tool.DevToolLayout
@@ -10,8 +12,7 @@ import com.maximbircu.devtools.common.presentation.tools.enum.EnumTool
 import com.maximbircu.devtools.common.presentation.tools.enum.EnumToolPresenter
 import com.maximbircu.devtools.common.presentation.tools.enum.EnumToolView
 import kotlinx.android.synthetic.main.layout_dev_tool.view.devToolCard
-import kotlinx.android.synthetic.main.layout_enum_tool.view.currentValue
-import kotlinx.android.synthetic.main.layout_enum_tool.view.selectorContainer
+import kotlinx.android.synthetic.main.layout_enum_tool.view.contentContainer
 
 class EnumToolLayout(context: Context) : DevToolLayout<EnumTool>(context), EnumToolView {
     private val presenter = EnumToolPresenter.create(this)
@@ -20,13 +21,11 @@ class EnumToolLayout(context: Context) : DevToolLayout<EnumTool>(context), EnumT
     override fun onBind(tool: EnumTool) = presenter.onToolBind(tool)
 
     override fun showCompactOptionsSelector(tool: EnumTool, onNewOptionSelected: (String) -> Unit) {
-        selectorContainer.removeAllViews()
-        val chipsSelector = EnumToolChipsOptionSelectorLayout(context, tool, onNewOptionSelected)
-        selectorContainer.addView(chipsSelector)
+        setContentView(EnumToolChipsOptionSelectorLayout(context, tool, onNewOptionSelected))
     }
 
     override fun showConfigurationValue(value: String) {
-        currentValue.text = value
+        setContentView(TextView(context).apply { text = value })
         devToolCard.setOnClickListener(presenter::onToolClick)
     }
 
@@ -38,5 +37,10 @@ class EnumToolLayout(context: Context) : DevToolLayout<EnumTool>(context), EnumT
             presenter::onNegativeButtonClick,
             onNewOptionSelected
         ).show()
+    }
+
+    private fun setContentView(contentView: View) {
+        contentContainer.removeAllViews()
+        contentContainer.addView(contentView)
     }
 }
