eea50a0629c875f437687598213bf58a2cc7bf32
Alexandr Vdovicenco <vdovicenco.alexandr@gmail.com>
2020-06-27 04:44:53 +0300

    Add constraints to loaded view in reused devtool

--- devtools/ios/DevtoolsFramework/Presentation/DevToolView.swift
+++ devtools/ios/DevtoolsFramework/Presentation/DevToolView.swift
@@ -24,11 +24,19 @@ public class DevToolView: UIView, NibLoadable {
         let toggleView = super.awakeAfter(using: aDecoder) as! UIView
         guard !toggleView.translatesAutoresizingMaskIntoConstraints else { return self }
         let devToolView = DevToolView.instantiate() as! DevToolView
-        devToolView.containerView.addSubview(toggleView)
-
         transferProperties(to: devToolView)
         let replacementConstraints = reparentedConstraints(to: devToolView)
         devToolView.addConstraints(replacementConstraints)
+
+        devToolView.containerView.transferProperties(to: toggleView)
+//        replacementConstraints = devToolView.containerView.reparentedConstraints(to: toggleView)
+//        toggleView.addConstraints(replacementConstraints)
+        devToolView.containerView.addSubview(toggleView)
+
+        toggleView.topAnchor.constraint(equalTo: devToolView.topAnchor, constant: 0).isActive = true
+        toggleView.bottomAnchor.constraint(equalTo: devToolView.bottomAnchor, constant: 0).isActive = true
+        toggleView.leadingAnchor.constraint(equalTo: devToolView.leadingAnchor, constant: 0).isActive = true
+        toggleView.trailingAnchor.constraint(equalTo: devToolView.trailingAnchor, constant: 0).isActive = true
 
         return devToolView
     }
