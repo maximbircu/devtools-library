eb446ebf27e3f51480ebb8382831aecb3c8b4e63
Alexandr Vdovicenco <vdovicenco.alexandr@gmail.com>
2021-03-06 21:01:55 +0200

    Fix iOS store for saving tool value

--- samples/ios/DevToolsApp/ViewController.swift
+++ samples/ios/DevToolsApp/ViewController.swift
@@ -5,14 +5,25 @@ import devtools
 class ViewController: UIViewController {
 
     @IBOutlet var toggleView: ToggleView!
+    let tool = ToggleTool(default: false)
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     override func viewWillAppear(_ animated: Bool) {
-        let tool = ToggleTool(default: false)
-        tool.value = false
+        tool.key = "toggle-tool"
+        tool.containerName = "Test"
+        tool.title = "Toggle tool"
+        tool.description = "A boolean configuration value dev tool"
+        tool.canBeDisabled = true
+        tool.defaultEnabledValue = false
+        tool.isCritical = false
+        tool.restorePersistedState()
         toggleView.bind(tool: tool)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        tool.persistState()
     }
 }
