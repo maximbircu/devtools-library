1d9069b918192a69acd024d97dce3c0c44d5806c
Maxim Bircu <maximbircu@gmail.com>
2021-08-04 23:59:36 +0300

    Cover new logic with unit tests

--- devtools/common/src/commonTest/kotlin/com/maximbircu/devtools/common/presentation/configscreen/ConfigScreenPresenterImplTest.kt
+++ devtools/common/src/commonTest/kotlin/com/maximbircu/devtools/common/presentation/configscreen/ConfigScreenPresenterImplTest.kt
@@ -7,15 +7,18 @@ import com.maximbircu.devtools.common.mvp.BasePresenterTest
 import com.maximbircu.devtools.common.presentation.list.DevToolsListView
 import com.maximbircu.devtools.common.utils.mockk
 import io.mockk.every
+import io.mockk.slot
 import io.mockk.verify
 import kotlin.test.Test
+import kotlin.test.assertTrue
 
 class ConfigScreenPresenterImplTest :
     BasePresenterTest<ConfigScreenView, ConfigScreenPresenter>(mockk()) {
     private val devTools: DevTools = mockk()
     private val devToolsList: DevToolsListView = mockk()
+    private val router: ConfigurationScreenRouter = mockk(relaxed = true)
 
-    override fun createPresenter() = ConfigScreenPresenter.create(view, devTools, devToolsList)
+    override fun createPresenter(): ConfigScreenPresenter = createPresenter(router)
 
     @Test
     fun `shows dev tools on create`() {
@@ -31,9 +34,71 @@ class ConfigScreenPresenterImplTest :
     }
 
     @Test
+    fun `restores persisted state on destroy`() {
+        presenter.onDestroy()
+
+        verify { devTools.restorePersistedState() }
+    }
+
+    @Test
+    fun `shows discard dialog if there are unsaved changes`() {
+        val router = RouterStub()
+        presenter = createPresenter(router)
+        val tools = mapOf<String, DevTool<Boolean>>("first-tool" to createTool())
+        every { devTools.tools } returns tools
+        every { devTools.thereExistUnsavedChanges } returns true
+        presenter.onCreate()
+
+        router.onBack?.invoke()
+
+        verify { view.showConfirmationDialog(any()) }
+    }
+
+    @Test
+    fun `returns false on back if there are no unsaved changes`() {
+        val router = RouterStub()
+        presenter = createPresenter(router)
+        val tools = mapOf<String, DevTool<Boolean>>("first-tool" to createTool())
+        every { devTools.tools } returns tools
+        every { devTools.thereExistUnsavedChanges } returns false
+        presenter.onCreate()
+
+        assertTrue(router.onBack?.invoke() == false)
+    }
+
+    @Test
+    fun `closes screen on confirmation`() {
+        val router = RouterStub()
+        presenter = createPresenter(router)
+        val tools = mapOf<String, DevTool<Boolean>>("first-tool" to createTool())
+        every { devTools.tools } returns tools
+        every { devTools.thereExistUnsavedChanges } returns true
+        val slot = slot<() -> Unit>()
+        every { view.showConfirmationDialog(capture(slot)) } answers { slot.captured.invoke() }
+        presenter.onCreate()
+
+        router.onBack?.invoke()
+
+        verify { router.closeScreenListener() }
+    }
+
+    @Test
     fun `persists tool state on apply config`() {
         presenter.onApplyConfig()
 
         devTools.persistToolsState()
+    }
+
+    private fun createPresenter(router: ConfigurationScreenRouter): ConfigScreenPresenter {
+        return ConfigScreenPresenter.create(view, devTools, devToolsList, router)
+    }
+}
+
+private class RouterStub : ConfigurationScreenRouter {
+    override var onBack: (() -> Boolean)? = null
+    val closeScreenListener: () -> Unit = mockk(relaxed = true)
+
+    override fun closeScreen() {
+        closeScreenListener()
     }
 }
