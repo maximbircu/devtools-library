99a63964667314ff5f552a115e26fa41ef308aa5
Cristian Lupu <lupucristiancptc@gmail.com>
2022-03-19 12:01:03 +0200

    Configure SwiftLint paths

--- devtools/ios/DevtoolsFramework/Extensions/DevToolsExtensions.swift
+++ devtools/ios/DevtoolsFramework/Extensions/DevToolsExtensions.swift
@@ -1,9 +1,11 @@
 import devtools
 
 public struct Devtools {
-    public static func create(name: String,
-                       devToolsSource: DevToolsSource...,
-                       onConfigUpdate: @escaping (Bool) -> Void = { _ in }) -> DevTools {
+    public static func create(
+        name: String,
+        devToolsSource: DevToolsSource...,
+        onConfigUpdate: @escaping (Bool) -> Void = { _ in }
+    ) -> DevTools {
         let sources = KotlinArray(size: Int32(devToolsSource.count)) { index in
             return devToolsSource[Int(truncating: index)]
         }
